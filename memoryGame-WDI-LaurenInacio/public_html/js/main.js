ded9148eea917558879b14918df606e8e3975511
Lauren <lauren_f23@yahoo.com>
2017-09-07 02:47:02 -0400

    created game logic

--- memoryGame-WDI-LaurenInacio/public_html/js/main.js
+++ memoryGame-WDI-LaurenInacio/public_html/js/main.js
@@ -1,10 +1,25 @@
+console.log("up and running!"); //testing log
 
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+var cardOne = cards[0]; //store 1st item in array:index 0:can test different variables 
+var cardTwo = cards[2]; //store 3rd item in array:index 2:can test different variables 
 
-console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardFour);
+cardsInPlay.push(cardOne);
+console.log('User flipped Queen');
+
+
+cardsInPlay.push(cardTwo);
+console.log('User flipped King');
+
+
+
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1])
+        alert('You found a match!');
+    else{
+        alert('Sorry, try again.');
+    }
+} //end brackett for initial if statement
