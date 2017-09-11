9486232712aae4a3aec54a0daf6ebde89e9d801f
Lauren <lauren_f23@yahoo.com>
2017-09-11 13:05:24 -0400

    created game logic

--- memoryGame-WDI-LaurenInacio/public_html/js/main.js
+++ memoryGame-WDI-LaurenInacio/public_html/js/main.js
@@ -1,25 +1,30 @@
-console.log("up and running!"); //testing log
-
-
 var cards = ['queen','queen','king','king'];
 var cardsInPlay = [];
-var cardOne = cards[0]; //store 1st item in array:index 0:can test different variables 
-var cardTwo = cards[2]; //store 3rd item in array:index 2:can test different variables 
 
+var checkForMatch = function(){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        console.log("You've found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }
+    
+};
 
-cardsInPlay.push(cardOne);
-console.log('User flipped Queen');
-
-
-cardsInPlay.push(cardTwo);
-console.log('User flipped King');
-
-
-
+var flipCard = function(cardId){
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+};
 if(cardsInPlay.length === 2){
-    if(cardsInPlay[0] === cardsInPlay[1])
+    
+    if(cardsInPlay[0] === cardsInPlay[1]){
         alert('You found a match!');
+    }
     else{
         alert('Sorry, try again.');
     }
 } //end brackett for initial if statement
+flipCard(0);
+flipCard(2);
+
+
